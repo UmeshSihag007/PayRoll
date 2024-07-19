d69b6abf704656a903b4a367b0822031bcc196a5
Umesh Sihag 007 <147179697+UmeshSihag007@users.noreply.github.com>
2024-07-19 06:11:34 +0530

    dynamic documentAdd Working

--- ApwPayrollWebApp/EnumHelpers/EnumHelper.cs
+++ ApwPayrollWebApp/EnumHelpers/EnumHelper.cs
@@ -4,13 +4,19 @@ namespace ApwPayrollWebApp.EnumHelpers
 {
     public static class EnumHelper
     {
-        public static IEnumerable<LookUpDto> GetEnumValues<T>() where T : Enum
+        public static IEnumerable<EnumHelperDto> GetEnumValues<T>() where T : Enum
         {
             return Enum.GetValues(typeof(T))
                 .Cast<T>()
-                .Select(e => new LookUpDto { Id= Convert.ToInt32(e), Name = e.ToString() })
+                .Select(e => new EnumHelperDto { Id= Convert.ToInt32(e), Name = e.ToString() })
                 .ToList();
         }
+    }
+    public   class EnumHelperDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
     }
 
 }
