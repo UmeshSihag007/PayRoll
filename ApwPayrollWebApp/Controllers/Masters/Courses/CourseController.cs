1c00348b458e1737f47f35ac03c49cd0cd32412f
Umesh Sihag 007 <147179697+UmeshSihag007@users.noreply.github.com>
2024-07-25 14:09:09 +0530

    course   working

--- ApwPayrollWebApp/Controllers/Masters/Courses/CourseController.cs
+++ ApwPayrollWebApp/Controllers/Masters/Courses/CourseController.cs
@@ -24,7 +24,7 @@ public class CourseController : BaseController
     {
         await IntializeViewBag();
         var model = new EmployeeProfileModel();
-        if (id.HasValue && id!=0)
+        if (id.HasValue && id != 0)
         {
             var courseData = await _mediator.Send(new GetEmployeeCoursesQuery());
             var course = courseData.Data.FirstOrDefault(x => x.Id == id.Value);
@@ -44,10 +44,10 @@ public class CourseController : BaseController
     [HttpPost]
     public async Task<IActionResult> CreateCourse(EmployeeProfileModel employeeProfile)
     {
-         
+
         if (employeeProfile.createCourses.Id == 0 || employeeProfile.createCourses.Id == null)
         {
-            await _mediator.Send( employeeProfile.createCourses);
+            await _mediator.Send(employeeProfile.createCourses);
 
         }
         else
@@ -55,29 +55,45 @@ public class CourseController : BaseController
             await _mediator.Send(new UpdateEmployeeCourseCommand((int)employeeProfile.createCourses.Id, employeeProfile.createCourses));
 
 
-          
+
         }
 
         return RedirectToAction("CourseView");
 
     }
-    
+    [HttpPost]
+    public IActionResult UpdateStatus(int id, bool isActive)
+    {
+        try
+        {
+
+
+            return Json(new { success = false });
+        }
+        catch (Exception ex)
+        {
+            // Log the exception
+            return Json(new { success = false, message = ex.Message });
+        }
+    }
+
+
     public async Task<IActionResult> Delete(int id)
     {
         var data = await _mediator.Send(new DeleteEmployeeCourseCommand(id));
         return RedirectToAction("CreateCourse");
     }
 
-    private async Task  IntializeViewBag()
+    private async Task IntializeViewBag()
     {
         var courseList = await _mediator.Send(new GetEmployeeCoursesQuery());
 
-         
-        if(courseList.Data!=null  && courseList.Data.Count != 0)
+
+        if (courseList.Data != null && courseList.Data.Count != 0)
         {
             var employeeCourse = courseList.Data.ToList();
             ViewBag.Course = employeeCourse;
-        }   
+        }
 
 
 
